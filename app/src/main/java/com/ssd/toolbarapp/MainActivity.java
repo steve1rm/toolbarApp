9fc0496c14387bb395686e1fcf427122646aa31b
Steve <steve1.rm@gmail.com>
2015-11-08 01:08:29 +0700

    Added switch statement for selecting the different toolbar menu options.

--- app/src/main/java/com/ssd/toolbarapp/MainActivity.java
+++ app/src/main/java/com/ssd/toolbarapp/MainActivity.java
@@ -5,9 +5,9 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,6 +26,17 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        int menuItem = item.getItemId();
+
+        switch(menuItem) {
+            case R.id.action_search:
+                Toast.makeText(MainActivity.this, "Action Search", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.action_favourite:
+                Toast.makeText(MainActivity.this, "Action Favourites", Toast.LENGTH_SHORT).show();
+        }
+
         return super.onOptionsItemSelected(item);
     }
 }
